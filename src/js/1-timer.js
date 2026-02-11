31fb48efd84fd3031ea261b0fab91312eab722fa
Roman <romchuck.kuzyshyn@gmail.com>
2026-02-11 17:34:20 +0200

    1

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,8 +1,15 @@
 import flatpickr from 'flatpickr';
+import izitoast from 'izitoast';
 import 'flatpickr/dist/flatpickr.min.css';
+import 'izitoast/dist/css/iziToast.min.css';
 
 let userSelectedDate;
 const inputDate = document.querySelector('#datetime-picker');
+const toastOptions = {
+  title: 'Please choose a date in the future',
+  position: 'bottomRight',
+  timeout: 3000,
+};
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -12,6 +19,7 @@ const options = {
     const today = Date.now();
     userSelectedDate = Date.parse(selectedDates[0]);
     if (userSelectedDate < today) {
+      izitoast.error(toastOptions);
       return;
     }
   },
